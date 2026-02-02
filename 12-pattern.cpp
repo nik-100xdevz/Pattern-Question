0df244f3093adfe73eb3183b66ac550e6817c03a
nik-100xdevz <rainikhil760@gmail.com>
2026-02-02 08:15:30 +0530

    pattern 12 solvd

---
+++ 12-pattern.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+void print12(int n){
+    for(int i =1; i<=n;i++){
+        //number
+        for(int j = 1; j <= i; j++){
+            cout<<j;
+        }
+        for(int j = 1; j<=2 * (n-i);j++){
+            cout<<" ";
+        }
+        //number
+        for (int j = i;j>=1; j-- ){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    print12(n);
+    
+    
+    return 0;
+}
